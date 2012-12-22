72237977ae3ad03cf6a5d2d05b458c6ad9bf6d6d
ruemic <rumicdesign@gmail.com>
2012-12-22 03:08:22 -0800

    seed all 5000 words into db from txt file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,28 @@
 Word.delete_all
-
-def create_word(incorrect, correct)
-  Word.create! incorrect: incorrect, correct: correct
-  puts "#{incorrect}: #{correct}"
+open("public/wordz.txt") do |words|
+  words.read.each_line do |word|
+    correct, incorrect = word.chomp.split("->")
+    Word.create!(:incorrect => incorrect, :correct => correct)
+  end
 end
 
-create_word 'abandonned', 'abandoned'
-create_word 'aberation', 'aberration'
-create_word 'abilties', 'abilities'
-create_word 'abilty', 'ability'
-create_word 'abondon', 'abandon'
-create_word 'abbout', 'about'
-create_word 'abotu', 'about'
-create_word 'abscence', 'absence'
-create_word 'abondoned', 'abandoned'
-create_word 'abondoning', 'abandoning'
-create_word 'abondons', 'abandons'
-create_word 'aborigene', 'aborigine'
-create_word 'accesories', 'accessories'
-create_word 'accidant', 'accident'
-create_word 'abortificant', 'abortifacient'
+# def create_word(incorrect, correct)
+#   Word.create! incorrect: incorrect, correct: correct
+#   puts "#{incorrect}: #{correct}"
+# end
+
+# create_word 'abandonned', 'abandoned'
+# create_word 'aberation', 'aberration'
+# create_word 'abilties', 'abilities'
+# create_word 'abilty', 'ability'
+# create_word 'abondon', 'abandon'
+# create_word 'abbout', 'about'
+# create_word 'abotu', 'about'
+# create_word 'abscence', 'absence'
+# create_word 'abondoned', 'abandoned'
+# create_word 'abondoning', 'abandoning'
+# create_word 'abondons', 'abandons'
+# create_word 'aborigene', 'aborigine'
+# create_word 'accesories', 'accessories'
+# create_word 'accidant', 'accident'
+# create_word 'abortificant', 'abortifacient'
